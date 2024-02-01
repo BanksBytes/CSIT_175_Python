3c30738058cf4fe6736cda5021fa2edab8857f39
Jordan Banks <jbanks@proficio.com>
2024-02-01 15:50:01 -0800

    First excercise complete

---
+++ Lab1/multiply.py
@@ -0,0 +1,24 @@
+#!/user/bin/env python3
+
+#Jordan Banks
+#Palomar College CSIT 175
+#Lab 1 Excercise 1, multiple 2 numbers
+
+#Variables
+
+# declare variables
+number1 = 0
+number2 = 0
+product = 0
+
+# input
+number1 = int(input("Enter first number:"))
+number2 = int(input("Enter second number:"))
+
+# process
+
+product = number1 * number2
+
+# output
+
+print("The product is " + str(product))
