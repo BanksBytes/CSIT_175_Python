5a73e46cfceb1add6c92cdd792e2fff2c8e15020
Jordan Banks <jordanb80@live.com>
2024-02-18 13:43:52 -0800

    Changes to Excercise 3 , Heads and Tails flip, tested and now working

--- Lab03/HeadsTails.py
+++ Lab03/HeadsTails.py
@@ -9,8 +9,8 @@ import random
 # variables and constants
 MAX_FLIPS = 20
 MAX_VAL = 2
-heads = 0.0
-tails = 0.0
+heads = 0
+tails = 0
 pct_heads = 0.0
 pct_tails = 0.0
 i = 0
@@ -18,14 +18,26 @@ flip = 0
 
 for i in range(1, MAX_FLIPS + 1, 1):
     flip = random.randint(0,1)
-    if flip == int 1:
-        heads += 1 and print ("Heads!")
-    else:
-        tails += 1 and print("Tails!")
+    if flip == 1 :
+        heads += 1 
+        print("Heads")
+    if flip == 0 :
+        tails += 1
+        print("Tails")
 
 
+
+# Print out the total # of heads that was flipped
 print("Total heads: {0}".format(heads))
-
+# Get the percentage that heads was flipped 
 pct_heads = (heads / MAX_FLIPS) * 100.0
-
+# Print out the percentage that was gotten on the previous calculation
 print("Pct heads: %{0:.1f}".format(pct_heads))
+
+
+# Print out the total # of heads that was flipped
+print("Total tails: {0}".format(tails))
+# Get the percentage that tails was flipped 
+pct_tails = (tails / MAX_FLIPS) * 100.0
+# Print out the percentage that was gotten on the previous calculation
+print("Pct tails: %{0:.1f}".format(pct_tails))
