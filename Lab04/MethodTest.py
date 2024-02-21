6d2fe9b5dba021c9d9775ab891efd4d3d5150eb5
Jordan Banks <jbanks@proficio.com>
2024-02-20 21:40:22 -0800

    worked more on Methods file , almost finished

--- Lab04/MethodTest.py
+++ Lab04/MethodTest.py
@@ -13,10 +13,58 @@ def main():
     float_val = m.get_float_val("Enter a floating point number: ")
     print("Float value " + str(float_val) + " was entered.")
 
-    # you'll add your code below this to test the various other methods
+    # test the int_float_val() method
+    int_val = m.get_int_val("Enter Integer: ")
+    print("Integer value " + str(int_val) + " was entered.")
+
+    # test is_float()
+    str_val = input("Enter a float:" )
+    result = m.is_float(str_val)
+    if result == True:
+        print(str_val + " is a float.")
+    else:
+        print( str_val + " is NOT a float.")
 
 
-# if this is the main module, then execture main()
+
+    # test is_int()
+    str_val = input("Enter an Integer:" )
+    result = m.is_int(str_val)
+    if result == True:
+        print(str_val + " is an Integer.")
+    else:
+        print( str_val + " is NOT an Integer.")
+
+
+    # test find_string()
+    # Create a list of random stuff. Let's vary the letter case to test
+    # note the list is in square brackets. Strings are in quotes and
+    # separated by commas
+    
+    my_list = ["goat", "ZEBRA", "HorsE", "foX", "dog", "Cat"]
+    value = input("Enter a value to find: ")
+    i = m.find_string(my_list, value)
+    # did we find what we are looking for
+    if i >= 0:
+        print("Found " + value + " at index: " + str(i) )
+    else:
+        print( value + " NOT FOUND!")
+
+    # test find_string()
+    # Create a list of random stuff. Let's vary the letter case to test
+    # note the list is in square brackets. Strings are in quotes and
+    # separated by commas
+    
+    my_list = ["56", "6", "54", "789", "3", "99"]
+    value = input("Enter an Integer to find: ")
+    i = m.find_int(my_list, value)
+    # did we find what we are looking for
+    if i >= 0:
+        print("Found " + value + " at index: " + str(i) )
+    else:
+        print( value + " NOT FOUND!")
+
+# if this is the main module, then execute main()
 
 if __name__ == "__main__":
     main()
--- Lab04/Methods.py
+++ Lab04/Methods.py
@@ -25,3 +25,69 @@ def get_float_val(prompt):
     return value
 
 # end get_float_val()
+
+
+# Create a user-defined method to get an Integer value. Includes exception handling.
+# Uses a try/except block to validate input. Stays in loop until valid Integer value
+# is found
+def get_int_val(prompt):
+    is_num = False
+    str_val = input(prompt)
+    while is_num == False:
+        try:
+            value = int(str_val)
+            is_num = True
+        except:
+            print(str_val + " is not an integer!")
+            str_val = input(prompt) # try again!
+    # end while
+            
+    return value
+# end get_int_val()
+
+
+# user-defined function to test if an input value is a float.
+
+def is_float(value):
+    try:
+        num = float(value)
+        return True # if we make it here, the conversion is successful
+    except:
+        return False # conversion to float failed so return false
+# end is_float
+    
+
+# user-defined function to test if an input value is an Integer.
+
+def is_int(value):
+    try:
+        num = int(value)
+        return True # if we make it here, the conversion is successful
+    except:
+        return False # conversion to float failed so return false
+# end is_int
+    
+# user-defined function that is used as a search 
+def find_string(the_list, find_val):
+    list_len = len(the_list)
+    found = False
+    index = 0
+    while index < list_len and found == False:
+        # convert both strings to upper case for the comparison
+        if find_val.upper() == the_list[index].upper():
+            return index
+        index += 1
+    return -1
+# end find_string
+
+# user-defined function to search for an integer
+
+def find_int(int_list, find_val):
+    list_len = len(int_list)
+    found = False
+    index = 0
+    while index < list_len and found == False:
+        if find_val() == int_list[index]:
+            return index
+        index += 1
+    return -1
