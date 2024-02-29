87e58ff4721f096cea950d7afd9c31e00e77e7cb
Jordan Banks <jbanks@proficio.com>
2024-02-29 08:44:29 -0800

    changes to excercise 1

--- Lab05/syntax_errors.py
+++ Lab05/syntax_errors.py
@@ -25,23 +25,23 @@ def main():
 # If statement should be terminated with a colon ":"
 #    if x > y
     if x > y:
-            print("x + os the greater than y")
+            print("x is the greater than y")
 # Else statement should be terminated with a colon ":"
 #    else
     else:
-        print("y + is greater than x")
+        print("y is greater than x")
 # for statement should be terminated with a colon ":"    
 #    for i in range(5)
     for i in range(5):
         print(i)
 
-        input = "what's your favorite season of the year?"
+    input = "what's your favorite season of the year?"
 # Incorrect Indentation here
 #            season = input
-        season = input
+    season = input
 # Incorrect Indentation here
 #            print(season)
-        print(season)
+    print(season)
 
 # if statement should be terminated with a colon ":", also should be using a comparison operator "==" instead of the assignment operator "="        
 #if __name__="__main__"
